27f536f89dbb16ac1ae68b2ae34c670ef2160ee1
Cassandra Culkins and Mao Thygesen <cassy.culkins@gmail.com>
2016-03-29 16:37:18 -0700

    add new article button functionality

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,11 @@ export default Ember.Route.extend({
     return this.store.findAll('article');
   },
   actions: {
+    save3(params) {
+     var newArticle = this.store.createRecord('article', params);
+     newArticle.save();
+     this.transitionTo('index');
+   },
     destroyArticle(article) {
       article.destroyRecord();
       this.transitionTo('admin');
